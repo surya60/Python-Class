0057a77e6d241f41b218ea1dee15290c89b2affd
surya teja reddy uppunuthula <30438356+surya60@users.noreply.github.com>
2018-03-19 16:27:05 -0400

    Add files via upload

---
+++ assign6.py
@@ -0,0 +1,10 @@
+def main():
+ pres = "Bush"
+ college = "Yale"
+ presAlmaMater(pres, college)
+ pres = "Obama"
+ college = "Columbia"
+ presAlmaMater(pres, college)
+def presAlmaMater(pres, college):
+ print("President", pres, "is a graduate of", college + '.')
+main()
